28a197bd15f92231ce36a5ef817442d7cde70c27
KenMR <kmr5591@gmail.com>
2023-02-27 22:36:26 -0500

    Enclose retrieve weather function in a module

--- weather.js
+++ weather.js
@@ -1,14 +1,17 @@
-async function retrieveWeather () {
-  // let searchTerm = `${search.value}`;
-  let searchTerm;
-  if (!searchTerm) {
-    searchTerm = 'New York';
+const weather = (() => {
+  const retrieveWeather = async () => {
+    // let searchTerm = `${search.value}`;
+    let searchTerm;
+    if (!searchTerm) {
+      searchTerm = 'New York';
+    }
+    const weatherPage = document.getElementById('weather-page');
+    const query = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&APPID=55ef89a82f7996b6a371e494df3d95f8
+    `, {mode: 'cors'})
+    const data = await query.json();
+    console.log(data);
   }
-  const weatherPage = document.getElementById('weather-page');
-  const query = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&APPID=55ef89a82f7996b6a371e494df3d95f8
-  `, {mode: 'cors'})
-  const data = await query.json();
-  console.log(data);
-}
+  return { retrieveWeather };
+})();
 
-retrieveWeather();
+weather.retrieveWeather();
