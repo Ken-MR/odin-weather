88e20fdf8b48c1dee6cf799b62604ab8cde71256
KenMR <kmr5591@gmail.com>
2023-03-01 18:30:57 -0500

    Update javascript to display information on webpage

--- src/page-control.js
+++ src/page-control.js
@@ -24,14 +24,52 @@ const DOMControl = (() => {
     let tempInfo = document.getElementById("temp");
     let icon = document.getElementById("weather-icon");
 
+    let feelsLikeInfo = document.getElementById("feels-like");
+    let humidityInfo = document.getElementById("humidity");
+    let windSpeedInfo = document.getElementById("wind-speed");
+
+    let timeCards = document.getElementById("time-cards");
+
     for (let i = 0; i < forecast.location.length; i++) {
       locationInfo.appendChild(document.createTextNode(`${forecast.location[i]}`));
       locationInfo.appendChild(document.createElement('br'));
     }
     weatherInfo.appendChild(document.createTextNode(`${forecast.weather}`));
     timeInfo.appendChild(document.createTextNode(`${forecast.time}`));
-    tempInfo.appendChild(document.createTextNode(`${forecast.tempF}`));
     icon.src = forecast.icon;
+
+    humidityInfo.appendChild(document.createTextNode(`${forecast.humid} %`));
+    windSpeedInfo.appendChild(document.createTextNode(`${forecast.wind[2]} `));
+
+    if (currentTempUnit === 'F') {
+      tempInfo.appendChild(document.createTextNode(`${forecast.tempF} °F`));
+      feelsLikeInfo.appendChild(document.createTextNode(`${forecast.feelsLikeF} °F`));
+      windSpeedInfo.appendChild(document.createTextNode(`${forecast.wind[0]} mph`));
+    }
+    else {
+      tempInfo.appendChild(document.createTextNode(`${forecast.tempC} °C`));
+      feelsLikeInfo.appendChild(document.createTextNode(`${forecast.feelsLikeC} °C`));
+      windSpeedInfo.appendChild(document.createTextNode(`${forecast.wind[1]} kph`));
+    }
+
+    if (currentTimeFrame === 'daily') {
+      for (let i = 0; i < forecast.dailyForecast.length; i++) {
+        let forecastElement = document.createElement('div');
+        let forecastTime = document.createElement('p');
+        let forecastTemp = document.createElement('h1');
+
+        forecastElement.appendChild(forecastTime);
+        forecastElement.appendChild(forecastTemp);
+        forecastElement.setAttribute('class', 'day-card');
+
+        forecastTime.appendChild(document.createTextNode(forecast.dailyForecast[i].date));
+        (currentTempUnit === 'F' ? 
+        forecastTemp.appendChild(document.createTextNode(`${forecast.dailyForecast[i].tempF} °F`)) 
+        : forecastTemp.appendChild(document.createTextNode(`${forecast.dailyForecast[i].tempC} °C`)));
+        timeCards.appendChild(forecastElement);
+      }
+    }
+
   }
   return { pageUpdates };
 })();
