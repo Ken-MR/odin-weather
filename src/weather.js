675eb4de4a253c2245de652d01c53ad9a3e887ae
KenMR <kmr5591@gmail.com>
2023-03-01 22:10:35 -0500

    Adjust element locations and UI

--- src/weather.js
+++ src/weather.js
@@ -53,17 +53,22 @@ export const weather = (() => {
 
   let searchTerm;
   const retrieveWeather = async () => {
-    if (!searchTerm) {
-      searchTerm = 'New York';
+    try {
+      if (!searchTerm) {
+        searchTerm = 'New York';
+      }
+      else {
+        searchTerm = search.value;
+      }
+      const query = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=e3b792acc4dc4d7593b232955232802&q=${searchTerm}&days=8&aqi=no&alerts=no
+      `, {mode: 'cors'});
+      const data = await query.json();
+      console.log(data);
+      parseWeather(data);
+    } 
+    catch (err) {
+      alert('Invalid location entered!');
     }
-    else {
-      searchTerm = search.value;
-    }
-    const query = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=e3b792acc4dc4d7593b232955232802&q=${searchTerm}&days=8&aqi=no&alerts=no
-    `, {mode: 'cors'});
-    const data = await query.json();
-    console.log(data);
-    parseWeather(data);
   }
 
   const parseWeather = (data) => {
